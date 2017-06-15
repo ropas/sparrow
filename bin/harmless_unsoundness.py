f34c4eb00b0a186de94c7e106ac91d63e861a4b6
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2017-06-15 11:32:45 +0900

    Enforce PEP8 for Python scripts

--- bin/harmless_unsoundness.py
+++ bin/harmless_unsoundness.py
@@ -9,64 +9,74 @@
 ## See the LICENSE file for details.                                   ##
 ##                                                                     ##
 #########################################################################
-import sys, os, re
+import sys
+import os
+import re
 from sklearn import svm
-import numpy as np 
+import numpy as np
 import pickle
 
-sparrow_data_path = os.environ['SPARROW_DATA_PATH'] 
+sparrow_data_path = os.environ['SPARROW_DATA_PATH']
+
 
 def mkTrSet(fname):
-  f = open(fname,"r")
-  lines = f.readlines()
-  trset = []
-  for line in lines:
-    feat = []
-    tokens = re.split(r' |\t|\n',line)
-    tokens = filter(lambda x: x.strip() != '', tokens)
-    if int(tokens[len(tokens)-1]) == 1:   
-      tokens = tokens[1:len(tokens)-1]
-      for b in tokens:
-        feat.append(float(b))
-      trset.append(feat)
-  return trset
+    f = open(fname, "r")
+    lines = f.readlines()
+    trset = []
+    for line in lines:
+        feat = []
+        tokens = re.split(r' |\t|\n', line)
+        tokens = filter(lambda x: x.strip() != '', tokens)
+        if int(tokens[len(tokens) - 1]) == 1:
+            tokens = tokens[1:len(tokens) - 1]
+            for b in tokens:
+                feat.append(float(b))
+            trset.append(feat)
+    return trset
+
 
 def doOCSVM(trset):
-  # fit the model
-  clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
-  clf.fit(trset)
-  return clf
+    # fit the model
+    clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+    clf.fit(trset)
+    return clf
+
 
 def training():
-  loop_training = sparrow_data_path + "/unsound_loop.dat" 
-  lib_training = sparrow_data_path + "/data/unsound_lib.dat" 
-  loop_trset = mkTrSet(loop_training)
-  lib_trset = mkTrSet(lib_training)
-  clf_loop = doOCSVM(loop_trset)
-  clf_lib = doOCSVM(lib_trset)
-  return (clf_loop, clf_lib)
+    loop_training = sparrow_data_path + "/unsound_loop.dat"
+    lib_training = sparrow_data_path + "/data/unsound_lib.dat"
+    loop_trset = mkTrSet(loop_training)
+    lib_trset = mkTrSet(lib_training)
+    clf_loop = doOCSVM(loop_trset)
+    clf_lib = doOCSVM(lib_trset)
+    return (clf_loop, clf_lib)
+
 
 def is_harmless(clf, vec):
-  sys.stdout.flush()
-  result = clf.predict(np.array(vec).reshape(1,-1))
-  if result == 1: 
-    return True
-  else:
-    return False
+    sys.stdout.flush()
+    result = clf.predict(np.array(vec).reshape(1, -1))
+    if result == 1:
+        return True
+    else:
+        return False
+
 
 def store(clf, name):
-  with open(name, 'w') as f:    
-    pickle.dump(clf, f)
+    with open(name, 'w') as f:
+        pickle.dump(clf, f)
+
 
 def load(name):
-  with open(name, 'r') as f:
-    clf = pickle.load(f)
-  return clf
+    with open(name, 'r') as f:
+        clf = pickle.load(f)
+    return clf
+
 
 def main(argv):
-  (clf_loop, clf_lib) = training()
-  store(clf_loop, sparrow_data_path + "harmless_loop_clf") 
-  store(clf_lib, sparrow_data_path + "harmless_lib_clf") 
-   
+    (clf_loop, clf_lib) = training()
+    store(clf_loop, sparrow_data_path + "harmless_loop_clf")
+    store(clf_lib, sparrow_data_path + "harmless_lib_clf")
+
+
 if __name__ == "__main__":
-  main(sys.argv[1:])
+    main(sys.argv[1:])
